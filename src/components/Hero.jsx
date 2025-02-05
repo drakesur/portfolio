62f64ad350121f0258aaec1f937de9c6565ed65f
drakesur <89182474+drakesur@users.noreply.github.com>
2025-02-04 19:16:28 -0800

    Added Tech Stack Icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import "./hero.css";
-
+import StackIcon from "tech-stack-icons";
 function Hero() {
   return (
     <div className="hero">
@@ -7,6 +7,13 @@ function Hero() {
       <div className="hero-content">
         <h1>Drake Sur</h1>
         <p>Web Developer | Data Engineer</p>
+        <div className="tech-stack">
+          <StackIcon name="python" className="stack-icon" />
+          <StackIcon name="js" className="stack-icon" />
+          <StackIcon name="reactjs" className="stack-icon" />
+          <StackIcon name="html5" className="stack-icon" />
+          <StackIcon name="css3" className="stack-icon" />
+        </div>
       </div>
     </div>
   );
